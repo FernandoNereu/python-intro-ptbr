cb83b1c29cf08d1023811bbbb57c4a14680b52ea
Fernando Nereu de Souza <fernando.nereu@gmail.com>
2025-04-03 22:53:31 -0300

    Fixing Pipeline

--- test_hello.py
+++ test_hello.py
@@ -1,19 +1,23 @@
 import pytest
 from hello import hello_world, add_numbers, factorial, is_palindrome
 
+
 def test_hello_world():
     assert "CI/CD Test Successful" in hello_world()
+
 
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0.5, 0.5) == 1.0
 
+
 def test_factorial():
     assert factorial(5) == 120
     assert factorial(0) == 1
     with pytest.raises(ValueError):
         factorial(-1)
+
 
 def test_is_palindrome():
     assert is_palindrome("racecar") is True
